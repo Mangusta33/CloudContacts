192c62d3912e84843fb890d70f31645a0133cb85
Simone La Falce <lafalcesimone@gmail.com>
2017-03-20 15:51:32 +0100

    modified contacts apis

--- src/contacts.php
+++ src/contacts.php
@@ -14,7 +14,7 @@
         return $this->response->withJson($utenti);
     });
 
-    // Retrieve contacts with name
+    // Retrieve contacts with id
     $app->get('/contacts/[{id}]', function ($request, $response, $args) {
     $sth = $this->db->prepare("SELECT * FROM contact WHERE id=:id");
     $sth->bindParam("id", $args['id']);
@@ -23,28 +23,19 @@
     return $this->response->withJson($todos);
     });
 
-    /*// Retrieve user with id
-    $app->get('/contacts/[{id}]', function ($request, $response, $args) {
-        $sth = $this->db->prepare("SELECT * FROM contacts WHERE id=:id");
-        $sth->bindParam("id", $args['id']);
-        $sth->execute();
-        $todos = $sth->fetchObject();
-        return $this->response->withJson($todos);
-    });
-
-    // Add a new user
-    $app->post('/users', function ($request, $response) {
+    // Add a new contact
+    $app->post('/contacts', function ($request, $response) {
         $input = $request->getParsedBody();
-        $sql = "INSERT INTO user (name,surname) VALUES (:name,:surname)";
+        $sql = "INSERT INTO contact (telnumber,name,id_group) VALUES (:telnumber,:name, :id_group)";
         $sth = $this->db->prepare($sql);
+        $sth->bindParam("telnumber", $input['telnumber']);
         $sth->bindParam("name", $input['name']);
-        $sth->bindParam("surname", $input['surname']);
+        $sth->bindParam("id_group", $input['id_group']);
         $sth->execute();
         $input['id'] = $this->db->lastInsertId();
         return $this->response->withJson($input);
     });
-
-
+/*
     // DELETE a user with given id
     $app->delete('/users/[{id}]', function ($request, $response, $args) {
         $sth = $this->db->prepare("DELETE FROM user WHERE id=:id");
