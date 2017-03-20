3bf7a1bda0ae727d5b9d00cd0fcc075242cc720f
Danilo93x <butcher-93@hotmail.it>
2017-03-20 17:58:40 +0100

    get, post, delete, put completed in groups.php

--- src/groups.php
+++ src/groups.php
@@ -1,54 +1,53 @@
 <?php
 
-// get all user
+// get all groups
     $app->get('/groups', function ($request, $response, $args) {
         $sth = $this->db->prepare("SELECT * FROM progettoreti.group");
         $sth->execute();
         $utenti = $sth->fetchAll();
         return $this->response->withJson($utenti);
     });
- /*
-    // Retrieve user with id
-    $app->get('/users/[{id}]', function ($request, $response, $args) {
-         $sth = $this->db->prepare("SELECT * FROM user WHERE id=:id");
+
+    // Retrieve group with id
+    $app->get('/groups/[{id}]', function ($request, $response, $args) {
+         $sth = $this->db->prepare("SELECT * FROM progettoreti.group WHERE id=:id");
         $sth->bindParam("id", $args['id']);
         $sth->execute();
         $todos = $sth->fetchObject();
         return $this->response->withJson($todos);
     });
- 
-    // Add a new user
-    $app->post('/users', function ($request, $response) {
-        $input = $request->getParsedBody();
-        $sql = "INSERT INTO user (name,surname) VALUES (:name,:surname)";
-        $sth = $this->db->prepare($sql);
-        $sth->bindParam("name", $input['name']);
-        $sth->bindParam("surname", $input['surname']);
-        $sth->execute();
-        $input['id'] = $this->db->lastInsertId();
-        return $this->response->withJson($input);
-    });
-        
- 
-    // DELETE a user with given id
-    $app->delete('/users/[{id}]', function ($request, $response, $args) {
-         $sth = $this->db->prepare("DELETE FROM user WHERE id=:id");
-        $sth->bindParam("id", $args['id']);
-        $sth->execute();
-        $todos = $sth->fetchAll();
-        return $this->response->withJson($todos);
-    });
- 
-    // Update user with given id           FUNZIONA
-    $app->put('/users/[{id}]', function ($request, $response, $args) {
-        $input = $request->getParsedBody();
-        $sql = "UPDATE user SET name=:name,surname=:surname WHERE id=:id";
-        $sth = $this->db->prepare($sql);
-        $sth->bindParam("id", $args['id']);
-        $sth->bindParam("name", $input['name']);
-        $sth->bindParam("surname", $input['surname']);
-        $sth->execute();
-        $input['id'] = $args['id'];
-        return $this->response->withJson($input);
-    });
- */
+
+   // Add a new group
+   $app->post('/groups', function ($request, $response) {
+       $input = $request->getParsedBody();
+       $sql = "INSERT INTO progettoreti.group (name,id_user) VALUES (:name,:id_user)";
+       $sth = $this->db->prepare($sql);
+       $sth->bindParam("name", $input['name']);
+       $sth->bindParam("id_user", $input['id_user']);
+       $sth->execute();
+       $input['id'] = $this->db->lastInsertId();
+       return $this->response->withJson($input);
+   });
+
+
+   // DELETE a group with given id
+   $app->delete('/groups/[{id}]', function ($request, $response, $args) {
+        $sth = $this->db->prepare("DELETE FROM progettoreti.group WHERE id=:id");
+       $sth->bindParam("id", $args['id']);
+       $sth->execute();
+       $todos = $sth->fetchAll();
+       return $this->response->withJson($todos);
+   });
+
+   // Update group with given id           FUNZIONA
+   $app->put('/groups/[{id}]', function ($request, $response, $args) {
+       $input = $request->getParsedBody();
+       $sql = "UPDATE progettoreti.group SET name=:name,id_user=:id_user WHERE id=:id";
+       $sth = $this->db->prepare($sql);
+       $sth->bindParam("id", $args['id']);
+       $sth->bindParam("name", $input['name']);
+       $sth->bindParam("id_user", $input['id_user']);
+       $sth->execute();
+       $input['id'] = $args['id'];
+       return $this->response->withJson($input);
+   });
